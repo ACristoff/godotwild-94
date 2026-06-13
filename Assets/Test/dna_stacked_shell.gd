405a51457f6df6eb0480efdaaa3d644233ad1001
kinteyoi <59457560+kinteyoi@users.noreply.github.com>
2026-06-13 00:15:11 -0500

    DNA scene functional to a degree

--- Assets/Test/dna_stacked_shell.gd
+++ Assets/Test/dna_stacked_shell.gd
@@ -7,36 +7,81 @@ extends Node2D
 var current_frame = 0
 var start = false
 var setter = false
-
+@export var ID: int
 @export var anim_start_frame = 0
+
+var subquadrant_king
+var current_king
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_parent().get_parent().shells.append(self)
 	dna_allele_1.frame = anim_start_frame
 	dna_allele_2.frame = anim_start_frame
 	dna_shell_1.frame = anim_start_frame
 	dna_shell_2.frame = anim_start_frame
-
+	current_frame = anim_start_frame
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if start:
-		if setter:
-			$Timer.start()
-			setter = false
+	$Label.text = str(current_frame)
+	#dna_allele_1.frame = anim_start_frame
+	#dna_allele_2.frame = anim_start_frame
+	#dna_shell_1.frame = anim_start_frame
+	#dna_shell_2.frame = anim_start_frame
+	#current_frame = anim_start_frame
+	if current_frame >= 24:
+		dna_allele_2.z_index = 1
+		dna_shell_2.z_index = 1
 	else:
-		dna_allele_1.frame = anim_start_frame
-		dna_allele_2.frame = anim_start_frame
-		dna_shell_1.frame = anim_start_frame
-		dna_shell_2.frame = anim_start_frame
-		current_frame = anim_start_frame
-		if current_frame >= 24:
-			dna_allele_2.z_index = 1
-			dna_shell_2.z_index = 1
-		else:
-			dna_allele_2.z_index = -1
-			dna_shell_2.z_index = -1
-	
+		dna_allele_2.z_index = -1
+		dna_shell_2.z_index = -1
+func stop():
+	$Timer.stop()
+func play():
+	$Timer.start()
 
+func submit_to_the_king():
+	if is_instance_valid(current_king):
+		print("ID:", ID)
+		print("King ID:", current_king.ID)
+		var distance_from_royalty = ID - current_king.ID
+		if subquadrant_king == 0:
+			dna_allele_1.frame = 24 - distance_from_royalty * 2
+			dna_allele_2.frame = 24 - distance_from_royalty * 2
+			dna_shell_1.frame = 24 - distance_from_royalty * 2
+			dna_shell_2.frame = 24 - distance_from_royalty * 2
+			current_frame = 24 - distance_from_royalty * 2
+			dna_allele_1.frame -= 1
+			dna_allele_2.frame -= 1
+			dna_shell_1.frame -= 1
+			dna_shell_2.frame -= 1
+			current_frame -= 1
+		if subquadrant_king == 1:
+			dna_allele_1.frame = 24 - distance_from_royalty * 2
+			dna_allele_2.frame = 24 - distance_from_royalty * 2
+			dna_shell_1.frame = 24 - distance_from_royalty * 2
+			dna_shell_2.frame = 24 - distance_from_royalty * 2
+			current_frame = 24 - distance_from_royalty * 2
+		if subquadrant_king == 2:
+			dna_allele_1.frame = 24 - distance_from_royalty * 2
+			dna_allele_2.frame = 24 - distance_from_royalty * 2
+			dna_shell_1.frame = 24 - distance_from_royalty * 2
+			dna_shell_2.frame = 24 - distance_from_royalty * 2
+			current_frame = 24 - distance_from_royalty * 2
+			dna_allele_1.frame += 1
+			dna_allele_2.frame += 1
+			dna_shell_1.frame += 1
+			dna_shell_2.frame += 1
+			current_frame += 1
+	
+func face_forward():
+	dna_allele_1.frame = 24
+	dna_allele_2.frame =24
+	dna_shell_1.frame =24
+	dna_shell_2.frame =24
+	current_frame =24
 
 func _on_timer_timeout() -> void:
 	dna_allele_1.frame += 1
--- Assets/Test/dna_test_screen.gd
+++ Assets/Test/dna_test_screen.gd
@@ -1,14 +1,41 @@
 extends Node2D
-
-
+@onready var area_2d: Area2D = $DNA/Area2D
+@onready var collision_shape_2d: CollisionShape2D = $DNA/Area2D/CollisionShape2D
+var current_hover
+var shells = []
+var detect_hover = false
+var height
+var spacing
+var sub_spacing
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	height = collision_shape_2d.shape.size.y
+	spacing = height / 8
+	sub_spacing = spacing / 3
+	print(shells)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if detect_hover:
+		var local_y = get_global_mouse_position().y - area_2d.global_position.y
+		local_y += height / 2.0
+		var quadrant = int(local_y / spacing)
+		quadrant = clamp(quadrant, 0, 7)
+		var within_quadrant = fmod(local_y, spacing)
+		var subquadrant_size = spacing / 3.0
+		var subquadrant = int(within_quadrant / subquadrant_size)
+		subquadrant = clamp(subquadrant, 0, 2)
+		print("Quadrant:", quadrant, " Sub:", subquadrant)
+		var current_shell = shells[quadrant]
+		for i in shells:
+			i.subquadrant_king = subquadrant
+			if i == shells[quadrant]:
+				continue 
+			i.current_king = current_shell
+			i.submit_to_the_king()
+		current_shell.face_forward()
+	#print(get_global_mouse_position() - area_2d.position)
 
 
 func _on_button_pressed() -> void:
@@ -45,3 +72,17 @@ func _on_button_pressed() -> void:
 	$DNA2/DNAStackedShell6.setter = true
 	$DNA2/DNAStackedShell7.setter = true
 	$DNA2/DNAStackedShell8.setter = true
+
+
+func _on_area_2d_mouse_entered() -> void:
+	#print("hi")
+	for i in shells:
+		i.stop()
+	detect_hover = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	#print("hi")
+	for i in shells:
+		i.play()
+	detect_hover = false
