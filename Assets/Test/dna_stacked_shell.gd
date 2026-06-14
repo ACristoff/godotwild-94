394233264b1bcdd7b0650f2974f30659891af983
kinteyoi <59457560+kinteyoi@users.noreply.github.com>
2026-06-13 23:41:28 -0500

    dna edits for slot visuals, assets for battlefield

--- Assets/Test/dna_stacked_shell.gd
+++ Assets/Test/dna_stacked_shell.gd
@@ -1,4 +1,3 @@
-@tool
 extends Node2D
 @onready var dna_allele_1: Sprite2D = $DnaAllele1
 @onready var dna_allele_2: Sprite2D = $DnaAllele2
@@ -7,14 +6,22 @@ extends Node2D
 var current_frame = 0
 var start = false
 var setter = false
-@export var ID: int
+
+var test_slot_type
+
+@onready var color_rect: ColorRect = $ColorRect
+@onready var slot_titles: Sprite2D = $SlotTitles
+
+
 @export var anim_start_frame = 0
 
-var subquadrant_king
-var current_king
+var possible = ["atk", "hp", "trait", "def", "spec", "cd", "breed", "null"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	test_slot_type = possible.pick_random()
+	set_slot(test_slot_type)
+	await get_tree().process_frame
 	get_parent().get_parent().shells.append(self)
 	dna_allele_1.frame = anim_start_frame
 	dna_allele_2.frame = anim_start_frame
@@ -22,82 +29,98 @@ func _ready() -> void:
 	dna_shell_2.frame = anim_start_frame
 	current_frame = anim_start_frame
 	
+func set_frame(frame:int):
+	current_frame = frame
+
+	dna_allele_1.frame = frame
+	dna_allele_2.frame = frame
+	dna_shell_1.frame = frame
+	dna_shell_2.frame = frame
+
+	update_depth()
+	
+func update_depth():
+	if current_frame >= 29:
+		dna_allele_2.z_index = 1
+		dna_allele_1.z_index = -1
+	else:
+		dna_allele_2.z_index = -1
+		dna_allele_1.z_index = 1
+
+	if current_frame >= 23:
+		dna_shell_1.z_index = -2
+		dna_shell_2.z_index = 2
+	else:
+		dna_shell_1.z_index = 2
+		dna_shell_2.z_index = -2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.text = str(current_frame)
-	#dna_allele_1.frame = anim_start_frame
-	#dna_allele_2.frame = anim_start_frame
-	#dna_shell_1.frame = anim_start_frame
-	#dna_shell_2.frame = anim_start_frame
-	#current_frame = anim_start_frame
-	if current_frame >= 24:
-		dna_allele_2.z_index = 1
-		dna_shell_2.z_index = 1
-	else:
-		dna_allele_2.z_index = -1
-		dna_shell_2.z_index = -1
-func stop():
-	$Timer.stop()
-func play():
-	$Timer.start()
+	pass
+	#print(current_frame)
+	#if start:
+		#$Timer.start()
+		#start = false
+		#setter = true
+	#if !setter:
+		#dna_allele_1.frame = anim_start_frame
+		#dna_allele_2.frame = anim_start_frame
+		#dna_shell_1.frame = anim_start_frame
+		#dna_shell_2.frame = anim_start_frame
+		#current_frame = anim_start_frame
+		
+func set_slot(type):
+	match type:
+		"atk":
+			color_rect.modulate = Color.from_string("9e3434", Color.BLACK)
+			slot_titles.frame = 0
+			dna_shell_1.modulate = Color.from_string("9c2424", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("9c2424", Color.BLACK)
+		"hp":
+			color_rect.modulate = Color.from_string("98a83b", Color.BLACK)
+			slot_titles.frame = 1
+			dna_shell_1.modulate = Color.from_string("779422", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("779422", Color.BLACK)
+		"trait":
+			color_rect.modulate = Color.from_string("cf6142", Color.BLACK)
+			slot_titles.frame = 2
+			dna_shell_1.modulate = Color.from_string("d14830", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("d14830", Color.BLACK)
+		"def":
+			color_rect.modulate = Color.from_string("5c7fe7", Color.BLACK)
+			slot_titles.frame = 3
+			dna_shell_1.modulate = Color.from_string("5f70e0", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("5f70e0", Color.BLACK)
+		"spec":
+			color_rect.modulate = Color.from_string("eb6ec5", Color.BLACK)
+			slot_titles.frame = 5
+			dna_shell_1.modulate = Color.from_string("cc4bae", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("cc4bae", Color.BLACK)
+		"breed":
+			color_rect.modulate = Color.from_string("7c40ff", Color.BLACK)
+			slot_titles.frame = 6
+			dna_shell_1.modulate = Color.from_string("784bcc", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("784bcc", Color.BLACK)
+		"cd":
+			color_rect.modulate = Color.from_string("5dbcd4", Color.BLACK)
+			slot_titles.frame = 4
+			dna_shell_1.modulate = Color.from_string("56bccc", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("56bccc", Color.BLACK)
+		"null":
+			color_rect.modulate = Color.from_string("828282", Color.BLACK)
+			slot_titles.frame = 7
+			dna_shell_1.modulate = Color.from_string("3d3d3d", Color.BLACK)
+			dna_shell_2.modulate = Color.from_string("3d3d3d", Color.BLACK)
 
-func submit_to_the_king():
-	if is_instance_valid(current_king):
-		print("ID:", ID)
-		print("King ID:", current_king.ID)
-		var distance_from_royalty = ID - current_king.ID
-		if subquadrant_king == 0:
-			dna_allele_1.frame = 24 - distance_from_royalty * 2
-			dna_allele_2.frame = 24 - distance_from_royalty * 2
-			dna_shell_1.frame = 24 - distance_from_royalty * 2
-			dna_shell_2.frame = 24 - distance_from_royalty * 2
-			current_frame = 24 - distance_from_royalty * 2
-			dna_allele_1.frame -= 1
-			dna_allele_2.frame -= 1
-			dna_shell_1.frame -= 1
-			dna_shell_2.frame -= 1
-			current_frame -= 1
-		if subquadrant_king == 1:
-			dna_allele_1.frame = 24 - distance_from_royalty * 2
-			dna_allele_2.frame = 24 - distance_from_royalty * 2
-			dna_shell_1.frame = 24 - distance_from_royalty * 2
-			dna_shell_2.frame = 24 - distance_from_royalty * 2
-			current_frame = 24 - distance_from_royalty * 2
-		if subquadrant_king == 2:
-			dna_allele_1.frame = 24 - distance_from_royalty * 2
-			dna_allele_2.frame = 24 - distance_from_royalty * 2
-			dna_shell_1.frame = 24 - distance_from_royalty * 2
-			dna_shell_2.frame = 24 - distance_from_royalty * 2
-			current_frame = 24 - distance_from_royalty * 2
-			dna_allele_1.frame += 1
-			dna_allele_2.frame += 1
-			dna_shell_1.frame += 1
-			dna_shell_2.frame += 1
-			current_frame += 1
-	
-func face_forward():
-	dna_allele_1.frame = 24
-	dna_allele_2.frame =24
-	dna_shell_1.frame =24
-	dna_shell_2.frame =24
-	current_frame =24
-
-func _on_timer_timeout() -> void:
-	dna_allele_1.frame += 1
-	dna_allele_2.frame += 1
-	dna_shell_1.frame += 1
-	dna_shell_2.frame += 1
+func tick_forward():
 	current_frame += 1
-	if current_frame >= 24:
-		dna_allele_2.z_index = 1
-		dna_shell_2.z_index = 1
-	else:
-		dna_allele_2.z_index = -1
-		dna_shell_2.z_index = -1
-	if current_frame == 47:
-		dna_allele_1.frame = 0
-		dna_allele_2.frame = 0
-		dna_shell_1.frame = 0
-		dna_shell_2.frame = 0
+
+	if current_frame >= 48:
 		current_frame = 0
+
+	dna_allele_1.frame = current_frame
+	dna_allele_2.frame = current_frame
+	dna_shell_1.frame = current_frame
+	dna_shell_2.frame = current_frame
+
+	update_depth()
--- Assets/Test/dna_test_screen.gd
+++ Assets/Test/dna_test_screen.gd
@@ -7,9 +7,14 @@ var detect_hover = false
 var height
 var spacing
 var sub_spacing
+
+var king_index : int
+var king_offset : int
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	height = collision_shape_2d.shape.size.y
+	height *= scale.y
 	spacing = height / 8
 	sub_spacing = spacing / 3
 	print(shells)
@@ -27,19 +32,49 @@ func _process(delta: float) -> void:
 		var subquadrant = int(within_quadrant / subquadrant_size)
 		subquadrant = clamp(subquadrant, 0, 2)
 		print("Quadrant:", quadrant, " Sub:", subquadrant)
-		var current_shell = shells[quadrant]
-		for i in shells:
-			i.subquadrant_king = subquadrant
-			if i == shells[quadrant]:
-				continue 
-			i.current_king = current_shell
-			i.submit_to_the_king()
-		current_shell.face_forward()
-	#print(get_global_mouse_position() - area_2d.position)
+		king_index = quadrant
+		#for i in shells:
+			#i.subquadrant_king = subquadrant
+			#if i == shells[quadrant]:
+				#continue 
+			#i.current_king = current_shell
+			#i.submit_to_the_king()
+		#current_shell.face_forward()
+	
+		match subquadrant:
+			0:
+				king_offset = -1
+			1:
+				king_offset = 0
+			2:
+				king_offset = 1
+				
+		var center_frame = 29 + king_offset
+		for i in range(shells.size()):
+			var distance = i - king_index
+			var frame = center_frame - distance * 3
+			frame = wrapi(frame, 0, 48)
+			shells[i].set_frame(frame)
 
 
-func _on_button_pressed() -> void:
-	pass # Replace with function body.
+func _on_area_2d_mouse_entered() -> void:
+	#print("hi")
+	$Timer.stop()
+	detect_hover = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	$Timer.start()
+	detect_hover = false
+
+
+func _on_timer_timeout() -> void:
+	for i in shells:
+		i.tick_forward()
+
+
+func _on_start_button_pressed() -> void:
+	$Timer.start()
 	$DNA/DNAStackedShell.start = true
 	$DNA/DNAStackedShell2.start = true
 	$DNA/DNAStackedShell3.start = true
@@ -56,33 +91,3 @@ func _on_button_pressed() -> void:
 	$DNA/DNAStackedShell6.setter = true
 	$DNA/DNAStackedShell7.setter = true
 	$DNA/DNAStackedShell8.setter = true
-	$DNA2/DNAStackedShell.start = true
-	$DNA2/DNAStackedShell2.start = true
-	$DNA2/DNAStackedShell3.start = true
-	$DNA2/DNAStackedShell4.start = true
-	$DNA2/DNAStackedShell5.start = true
-	$DNA2/DNAStackedShell6.start = true
-	$DNA2/DNAStackedShell7.start = true
-	$DNA2/DNAStackedShell8.start = true
-	$DNA2/DNAStackedShell.setter = true
-	$DNA2/DNAStackedShell2.setter = true
-	$DNA2/DNAStackedShell3.setter = true
-	$DNA2/DNAStackedShell4.setter = true
-	$DNA2/DNAStackedShell5.setter = true
-	$DNA2/DNAStackedShell6.setter = true
-	$DNA2/DNAStackedShell7.setter = true
-	$DNA2/DNAStackedShell8.setter = true
-
-
-func _on_area_2d_mouse_entered() -> void:
-	#print("hi")
-	for i in shells:
-		i.stop()
-	detect_hover = true
-
-
-func _on_area_2d_mouse_exited() -> void:
-	#print("hi")
-	for i in shells:
-		i.play()
-	detect_hover = false
