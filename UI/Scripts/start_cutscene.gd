1012c61f6c437a0733ba36bf222bb0ad596ef991
kinteyoi <59457560+kinteyoi@users.noreply.github.com>
2026-06-18 15:00:34 -0500

    letter opening cutscene

---
+++ UI/Scripts/start_cutscene.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var hit_amnt = 0
+var letteropened = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	match  hit_amnt:
+		0:
+			hit_amnt += 1
+			$AnimationPlayer.play("Attempt1")
+		1:
+			hit_amnt += 1
+			$AnimationPlayer.play("Attempt2")
+		2:
+			hit_amnt += 1
+			$AnimationPlayer.play("OpenLetter")
+		3:
+			return
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "OpenLetter":
+		letteropened = true
+
+
+func _on_button_2_pressed() -> void:
+	if hit_amnt == 3 and letteropened == true:
+		$AnimationPlayer.play("ByeByeLetter")
