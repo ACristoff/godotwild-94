e6959ab0c1ef6d8a26da560b71d2cc1bb71c9d9b
kinteyoi <59457560+kinteyoi@users.noreply.github.com>
2026-06-20 17:04:40 -0500

    All the intro cutscenes in

--- UI/Scripts/battle_clash.gd
+++ UI/Scripts/battle_clash.gd
@@ -1,5 +1,6 @@
-extends Control
+extends CanvasLayer
 
+const YIP_VS_BONK = preload("uid://cgj1x1lw7b77j")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +10,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func play_sound():
+	AudMan.play_sfx_wav(YIP_VS_BONK, 0.0, false)
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
