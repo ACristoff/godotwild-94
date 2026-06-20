a3c9a871a0e0000cc5894adc6504935727002e34
p-dobb <fackn1234@gmail.com>
2026-06-20 11:18:54 -0700

    Add spawn animation for farm hub

--- World/yipee/yipee.gd
+++ World/yipee/yipee.gd
@@ -21,6 +21,8 @@ const YIP_BUFF = preload("uid://nkypm8ojycqe")
 @onready var health_UI: HealthBar = $HealthBar
 @onready var hover_area: Area2D = $HoverArea
 
+@onready var animation_player : AnimationPlayer = $ActionsAnim
+
 signal yip_hovered(yip_data: Yipee)
 signal yip_unhovered
 
