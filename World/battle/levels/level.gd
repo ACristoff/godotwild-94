2cd75365a8c74109c52d48a9b3c4a7d7bbb5243e
Alex Cristoffanini <acristoffanini@gmail.com>
2026-06-20 00:32:30 -0700

    level resource stuff

--- World/battle/levels/level.gd
+++ World/battle/levels/level.gd
@@ -8,3 +8,5 @@ extends Resource
 @export_group("Intro")
 @export var intro_animation: PackedScene
 @export var intro_timeline: DialogicTimeline
+@export var enemy_portait: Texture2D
+@export var enemy_color: Color
